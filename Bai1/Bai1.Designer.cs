9945a37fd8f8ea5d51c758b5baca8e70a32c69d0
Nhat Minh <nguyenminh08042006@gmail.com>
2025-12-03 18:08:53 +0700

    Bai1 và Bai7

---
+++ Bai1/Bai1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Bai1
+{
+    partial class Bai1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.RichTextBox rtxtHtmlContent;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.rtxtHtmlContent = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(699, 22);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "http://uit.edu.vn";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGet.Location = new System.Drawing.Point(717, 10);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(71, 28);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "GET";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // rtxtHtmlContent
+            // 
+            this.rtxtHtmlContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtxtHtmlContent.Location = new System.Drawing.Point(12, 44);
+            this.rtxtHtmlContent.Name = "rtxtHtmlContent";
+            this.rtxtHtmlContent.Size = new System.Drawing.Size(776, 394);
+            this.rtxtHtmlContent.TabIndex = 2;
+            this.rtxtHtmlContent.Text = "";
+            this.rtxtHtmlContent.TextChanged += new System.EventHandler(this.rtxtHtmlContent_TextChanged);
+            // 
+            // Bai1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtxtHtmlContent);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Bai1";
+            this.Text = "Bài 1: Hiển thị Nội dung HTML";
+            this.Load += new System.EventHandler(this.Bai1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ Bai7/DishControl.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using System.Threading.Tasks; 
+
+namespace Bai7
+{
+    public delegate void DishDeletedEventHandler(object sender, string dishId);
+
+    public delegate void DishSelectedEventHandler(object sender, Dish dish);
+
+    public partial class DishControl : UserControl
+    {
+        public event DishDeletedEventHandler DishDeleted;
+        public event DishSelectedEventHandler DishSelected;
+
+        public Dish CurrentDish { get; private set; }
+
+        public DishControl()
+        {
+            InitializeComponent();
+            btnDelete.Click += BtnDelete_Click;
+            this.Click += DishControl_Click;
+            pnlContainer.Click += DishControl_Click;
+        }
+
+        /// <summary>
+        /// Binds data from the Dish object to the controls.
+        /// </summary>
+        public void SetDishData(Dish dish)
+        {
+            CurrentDish = dish;
+            this.Tag = dish.Id;
+
+            lblName.Text = dish.Name;
+            lblPrice.Text = $"Giá: {dish.Price.ToString("N0")} VNĐ";
+            lblAddress.Text = $"Địa chỉ: {dish.Address}";
+            lblContributor.Text = $"Đóng góp: {dish.ContributorName ?? "N/A"}";
+
+            if (!string.IsNullOrEmpty(dish.ImageUrl))
+            {
+                LoadImageAsync(dish.ImageUrl);
+            }
+        }
+
+        /// <summary>
+        /// Loads the image asynchronously from the URL and displays it on the PictureBox.
+        /// </summary>
+        private async void LoadImageAsync(string imageUrl)
+        {
+            string encodedUrl = Uri.EscapeUriString(imageUrl);
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var imageBytes = await client.GetByteArrayAsync(encodedUrl);
+
+                    using (var ms = new MemoryStream(imageBytes))
+                    {
+                        picFoodImage.Image = Image.FromStream(ms);
+                        picFoodImage.SizeMode = PictureBoxSizeMode.Zoom;
+                    }
+                }
+                catch (HttpRequestException)
+                {
+                    picFoodImage.Image = null;
+                }
+                catch (Exception)
+                {
+                    picFoodImage.Image = null;
+                }
+            }
+        }
+
+        public void ShowDeleteButton(bool show)
+        {
+            btnDelete.Visible = show;
+        }
+
+        private void DishControl_Click(object sender, EventArgs e)
+        {
+            DishSelected?.Invoke(this, CurrentDish);
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (CurrentDish == null) return;
+
+            if (MessageBox.Show($"Bạn có chắc chắn muốn xóa món ăn '{CurrentDish.Name}'?", "Xác nhận xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+       
+                DishDeleted?.Invoke(this, CurrentDish.Id);
+            }
+        }
+    }
+}
